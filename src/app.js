7fac2d291c3133e9e9b2554a148c0838d668a84e
Adam <25955@edu.amw.gdynia.pl>
2026-06-07 17:33:07 +0200

    fix 2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 
+
+
+
 import expenseRoutes from "./api/routes/expense.routes.js";
 import exchangeRoutes from "./api/routes/exchange.routes.js";
 import categoryRoutes from "./api/routes/category.routes.js";
@@ -20,6 +23,17 @@ app.use("/exchange-rate", exchangeRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "App działa" });
+});
+
+app.use((err, req, res, next) => {
+  console.error("🔥 FULL ERROR:");
+  console.error(err);
+  console.error(err?.stack);
+
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack
+  });
 });
 
 export default app;
