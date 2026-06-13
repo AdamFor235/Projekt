66d18c736ab0073e6cd520f4fd769eb5123ae142
Adam <25955@edu.amw.gdynia.pl>
2026-06-13 17:30:28 +0200

    railway try1

--- src/config/database.js
+++ src/config/database.js
@@ -2,19 +2,21 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_NAME: process.env.DB_NAME,
+  DB_PASSWORD_EXISTS: !!process.env.DB_PASSWORD
+});
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 3306),
+    host: process.env.MYSQLHOST,
+    port: Number(process.env.MYSQLPORT),
     dialect: "mysql",
     logging: false,
-    dialectOptions: {
-      connectTimeout: 20000
-    }
   }
 );
 
